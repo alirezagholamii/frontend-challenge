7c99cf72a59db7958a1e91b66b94eea6e9fe1dc7
Alireza <alireza.gholami403@gmail.com>
2022-01-28 21:06:03 +0330

    style: fix indentation

--- task-two/index.js
+++ task-two/index.js
@@ -8,58 +8,48 @@ import { getUserByName, getUserInfractions } from './user-api.js';
  * @param {string} username
  * @param {function(string)} callback
  */
-function getReasonForWorstInfractionLinkified(username, callback)
-{
-	getUserByName(username, function (user)
-	{
-		getUserInfractions(user.id, function (result)
-		{
-			// find most recent infraction with most infraction points
-			let foundIndex = 0;
-			for (let i = result.length - 1; i >= 0; i--)
-			{
-				if (result[i].points > result[foundIndex].points)
-				{
-					foundIndex = i;
-				}
-			}
+function getReasonForWorstInfractionLinkified(username, callback) {
+  getUserByName(username, function (user) {
+    getUserInfractions(user.id, function (result) {
+      // find most recent infraction with most infraction points
+      let foundIndex = 0;
+      for (let i = result.length - 1; i >= 0; i--) {
+        if (result[i].points > result[foundIndex].points) {
+          foundIndex = i;
+        }
+      }
 
-			// replace urls by links
-			callback(result[foundIndex].reason.replace(
-				/\bhttps:\/\/\S+/,
-				match => '<a href="' + match + '">' + match + '</a>'
-			));
-		});
-	});
+      // replace urls by links
+      callback(result[foundIndex].reason.replace(
+        /\bhttps:\/\/\S+/,
+        match => '<a href="' + match + '">' + match + '</a>'
+      ));
+    });
+  });
 }
 
 /**
  * @param {string} name
  * @param {function(string)} callback
  */
-function getReasonForMostRecentInfractionLinkified(name, callback)
-{
-	getUserByName(name, function (user)
-	{
-		getUserInfractions(user.id, function (result)
-		{
-			// find most recent infraction
-			let foundIndex = 0;
-			for (let i = 1; i < result.length; i++)
-			{
-				if (result[i].id > result[foundIndex].id)
-				{
-					foundIndex = i;
-				}
-			}
+function getReasonForMostRecentInfractionLinkified(name, callback) {
+  getUserByName(name, function (user) {
+    getUserInfractions(user.id, function (result) {
+      // find most recent infraction
+      let foundIndex = 0;
+      for (let i = 1; i < result.length; i++) {
+        if (result[i].id > result[foundIndex].id) {
+          foundIndex = i;
+        }
+      }
 
-			// replace urls by links
-			callback(result[foundIndex].reason.replace(
-				/\bhttps:\/\/\S+/,
-				match => '<a href="' + match + '">' + match + '</a>'
-			));
-		});
-	});
+      // replace urls by links
+      callback(result[foundIndex].reason.replace(
+        /\bhttps:\/\/\S+/,
+        match => '<a href="' + match + '">' + match + '</a>'
+      ));
+    });
+  });
 }
 
 /**
@@ -67,16 +57,12 @@ function getReasonForMostRecentInfractionLinkified(name, callback)
  * @param {string} username
  * @returns {Promise.<Object>}
  */
-export function getRelevantInfractionReasons(username)
-{
-	return new Promise(function (resolve)
-	{
-		getReasonForWorstInfractionLinkified(username, function (worst)
-		{
-			getReasonForMostRecentInfractionLinkified(username, function (mostRecent)
-			{
-				resolve({mostRecent, worst});
-			});
-		});
-	});
+export function getRelevantInfractionReasons(username) {
+  return new Promise(function (resolve) {
+    getReasonForWorstInfractionLinkified(username, function (worst) {
+      getReasonForMostRecentInfractionLinkified(username, function (mostRecent) {
+        resolve({ mostRecent, worst });
+      });
+    });
+  });
 }
