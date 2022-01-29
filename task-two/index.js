9814c1dc11ecaa61ee81c66752059e5a82712551
Alireza <alireza.gholami403@gmail.com>
2022-01-29 23:36:34 +0330

    fix: add exception handling

--- task-two/index.js
+++ task-two/index.js
@@ -4,7 +4,17 @@ import userService from './user-service.js'
 export const getRelevantInfractionReasons = async (username) => {
   try {
     const user = await userApi.getUserByNameAdapter(username)
+    if (user === null) {
+      throw 'user not found'
+    }
+
     const userInfractions = await userApi.getUserInfractionsAdapter(user.id)
+    if (userInfractions === null) {
+      throw 'infractions not found'
+    }
+    if (userInfractions.length === 0) {
+      return { mostRecent: null, worst: null }
+    }
 
     const worst = userService.getReasonForWorstInfractionLinkified(userInfractions)
     const mostRecent = userService.getReasonForMostRecentInfractionLinkified(userInfractions)
